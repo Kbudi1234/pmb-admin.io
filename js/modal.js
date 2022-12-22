40a2bacd06b66e124f19dd8ef69763156e3ca9f1
Kbudi1234 <budi48737@gmail.com>
2022-12-22 15:32:53 +0700

    Menambahkan File Admin Keuangan

--- js/modal.js
+++ js/modal.js
@@ -19,3 +19,17 @@ document.addEventListener("DOMContentLoaded", function () {
     false
   );
 });
+
+var dropdown = document.getElementsByClassName("dropdown-btn");
+var i;
+
+for (i = 0; i < dropdown.length; i++) {
+  dropdown[i].addEventListener("click", function () {
+    var dropdownContent = this.nextElementSibling;
+    if (dropdownContent.style.display === "block") {
+      dropdownContent.style.display = "none";
+    } else {
+      dropdownContent.style.display = "block";
+    }
+  });
+}
